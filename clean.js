925ad58e42a049bdfb6f5e3ac107220130556814
ErikBoesen <me@erikboesen.com>
2018-11-21 13:24:35 -0500

    Remove capitalization in navbar

--- clean.js
+++ clean.js
@@ -4,5 +4,5 @@ nav.removeChild(nav.childNodes[0]);
 var item = document.querySelector('nav[role="navigation"] ul li:nth-of-type(3)');
 var new_item = item.cloneNode(true);
 new_item.childNodes[0].href = '/';
-new_item.childNodes[0].textContent = 'HOME';
+new_item.childNodes[0].textContent = 'Home';
 nav.prepend(new_item);
