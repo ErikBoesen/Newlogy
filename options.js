5c1eec092641712af5e41c0bf91686fdba4347fe
ErikBoesen <me@erikboesen.com>
2018-11-23 14:05:42 -0500

    Automatically save options oninput

--- options.js
+++ options.js
@@ -1,7 +1,6 @@
 const browser = window.browser || window.chrome;
 
-// Saves options to chrome.storage
-function save_options() {
+function save() {
     var autoload = document.getElementById('autoload').checked;
     browser.storage.sync.set({
         autoload: autoload
@@ -15,14 +14,10 @@ function save_options() {
     });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-    browser.storage.sync.get({
-        autoload: true
-    }, function(items) {
-        document.getElementById('autoload').checked = items.autoload;
-    });
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+oninput = save;
+
+browser.storage.sync.get({
+    autoload: true
+}, function(items) {
+    document.getElementById('autoload').checked = items.autoload;
+});
