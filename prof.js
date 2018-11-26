63be32339165a7d1ce04f233f9ea0d372e7cc9f7
ErikBoesen <me@erikboesen.com>
2018-11-25 22:06:15 -0500

    Animate brand on my profile

--- prof.js
+++ prof.js
@@ -1,3 +1,4 @@
 let brand = document.createElement('img');
+brand.id = 'brand';
 brand.src = 'https://raw.githubusercontent.com/ErikBoesen/Newlogy/master/icons/64.png';
 document.getElementById('content-left').appendChild(brand);
