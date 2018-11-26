f63a8b498ea9ffe3aa2edb1a89a84ec4f7c62f16
ErikBoesen <me@erikboesen.com>
2018-11-25 21:56:11 -0500

    Add a logo to my own profile

---
+++ prof.js
@@ -0,0 +1,3 @@
+let brand = document.createElement('img');
+brand.src = 'https://raw.githubusercontent.com/ErikBoesen/Newlogy/master/icons/64.png';
+document.getElementById('content-left').appendChild(brand);
