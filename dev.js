ee8518d6727616f753b043200a17f75ba6261515
ErikBoesen <me@erikboesen.com>
2018-11-27 17:08:47 -0500

    Add hover text on developer badge

--- dev.js
+++ dev.js
@@ -1,4 +1,5 @@
 let brand = document.createElement('img');
 brand.id = 'brand';
+brand.title = 'Newlogy developer';
 brand.src = 'https://raw.githubusercontent.com/ErikBoesen/Newlogy/master/icons/64.png';
 document.getElementById('content-left').appendChild(brand);
