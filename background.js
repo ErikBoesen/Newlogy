1b1dce47d06d14dd10ab9db35c6b62a8e4cc0bcc
ErikBoesen <me@erikboesen.com>
2018-11-29 12:49:18 -0500

    Fix opening of uninstall form

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(function(details) {
     if (details.reason == 'install') {
         // On first install, set uninstall URL if browser supports it
         if (chrome.runtime.setUninstallURL) {
-            chrome.runtime.setUninstallURL(uninstallGoogleFormLink);
+            chrome.runtime.setUninstallURL(FORM_URL);
         }
     }
 });
