2cd0d8bdd8b5e3771b8047d7d39c677c79075a0c
ErikBoesen <me@erikboesen.com>
2018-11-29 12:46:45 -0500

    Add first background script

---
+++ background.js
@@ -0,0 +1,10 @@
+const FORM_URL = 'https://goo.gl/forms/HxoIETyK0SeLPDVz1';
+
+chrome.runtime.onInstalled.addListener(function(details) {
+    if (details.reason == 'install') {
+        // On first install, set uninstall URL if browser supports it
+        if (chrome.runtime.setUninstallURL) {
+            chrome.runtime.setUninstallURL(uninstallGoogleFormLink);
+        }
+    }
+});
