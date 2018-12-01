7e889d3fda50ed1e08de80b677ae4379584a77d5
ErikBoesen <me@erikboesen.com>
2018-12-01 10:29:43 -0500

    Clean up autoload logic

--- clean.js
+++ clean.js
@@ -16,10 +16,8 @@ browser.storage.sync.get(['floating_header', 'autoload'], function(items) {
     }
     if (items.autoload || items.autoload == undefined) {
         // Automatically load more posts when scrolled to bottom of a feed page
-        // TODO: allow disabling this in settings
         window.onscroll = function() {
-            // TODO: also check if current scroll speed will bring us to the bottom
-            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - (window.innerHeight)) {
+            if (2 * window.innerHeight + window.scrollY >= document.body.offsetHeight) {
                 try {
                     // Simulate clicking "More" button
                     document.querySelector('.sEdgeMore-processed').click();
